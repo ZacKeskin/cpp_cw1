94c7fc32ba00094cc0b338be54ed60e28193faac
Zac Keskin <zackeskin@outlook.com>
2018-02-24 20:00:26 +0000

    Got working 2D vector to read points in from fixed.txt (string format so far) using boost::split

--- src/pbreg.cc
+++ src/pbreg.cc
@@ -2,8 +2,9 @@
 #include <fstream>
 #include <string>
 #include <boost/algorithm/string.hpp>
-#include "../headers/exceptions.h"
+#include <boost/program_options.hpp>
 
+#include "../headers/exceptions.h"
 
 using std::cout;
 using std::cout;
@@ -11,9 +12,11 @@ using std::endl;
 
 int main(){
 
+std::vector<std::vector<string> > two_D_array;
+
 // Create a filestream object (read only)
 ifstream myfile;
-string filepath = "../test/fixed.txt";
+string filepath = "../Data/fixed.txt";
 // Use it to open our text file
 myfile.open (filepath, ios::out); 
 
@@ -23,31 +26,36 @@ if (myfile.is_open())
     {
 
      string line;    
-     int row = 0;
-     int col = 0;  
+
      // Read all lines in the file
      while ( getline (myfile,line) )
         {
+
         string strInput;
-        //cout << line << '\n';
-       
+        // Split each line into an array using tab delimiter
         std::string str = line;
         std::vector<std::string> tokens;
-
         boost::split(tokens, line, boost::is_any_of("\t"));
-         
-        for(int i=0; i<tokens.size(); i++)
-            {
-            std::cout << tokens[i]  << '\n';
-            };
-
-        row += 1;
-        
+        two_D_array.push_back(tokens); 
         }
+
     }
 else
+    {
     cout << "File " << filepath  << " not found" << endl;
-    
+    }
+
+
+for(int row=0; row < two_D_array.size(); row++)
+    {
+    for (int col=0; col < two_D_array[row].size(); col++)
+        {
+            std::cout << two_D_array[row][col] ;
+        }
+        std::cout << endl;
+    };
+
+
 return 0;
 
 
